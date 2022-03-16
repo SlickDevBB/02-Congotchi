fa4fd9fe04efabf63f2f047b53a19adf030d12af
SlickDevBB <NA@NA>
2022-03-16 11:05:45 +0800

    new commit

--- app/src/game/objects/world-map.ts
+++ app/src/game/objects/world-map.ts
@@ -52,15 +52,6 @@ export class WorldMap extends Phaser.GameObjects.Image {
         // create cursor control
         this.cursorKeys = this.scene.input.keyboard.createCursorKeys();
 
-        // create the debug text
-        // const fontHeight = getGameHeight(this.scene)/50*1.25;
-        // this.debugText = this.scene.add.text(10, 100, 'Debug Info', { font: fontHeight+'px Courier', color: '#ff0000' });
-        // this.debugText.setScrollFactor(0);
-        // this.debugText.setStroke("#ffffff", 4);
-        // // this.debugText.setShadow(3, 3, '#ffffff');
-        // this.debugText.setVisible(process.env.NODE_ENV === 'development');
-        // this.debugText.setDepth(10000);
-
         // get the main scene camera and set its initial scroll position
         this.worldCam = this.scene.cameras.main;
         this.worldCam.setBounds(0, 0, this.displayWidth, this.displayHeight);
@@ -207,18 +198,6 @@ export class WorldMap extends Phaser.GameObjects.Image {
         this.levelButtons.map( btn => {
             btn.update();
         })
-
-        // // render some debug info
-        // const pointer = this.scene.input.activePointer;
-
-        // this.debugText.setText([
-        //     'World Parameters:',
-        //     '  Ptr % Width:  ' + (this.scene.cameras.main.scrollX/this.worldWidth + pointer.x/this.worldWidth).toFixed(3),
-        //     '  Ptr % Height: ' + (this.scene.cameras.main.scrollY/this.worldHeight + pointer.y/this.worldHeight).toFixed(3),
-        //     '  Camera X: ' + (this.scene.cameras.main.scrollX/this.worldWidth).toFixed(3),
-        //     '  Camera Y: ' + (this.scene.cameras.main.scrollY/this.worldHeight).toFixed(3),
-        // ]);
-        // this.debugText.setPosition(0.2*this.displayWidth, 0.5*this.displayHeight);
 
     }
 
